4b308ff72e2d1fc03e03160bb7451216cf93a02b
AlinaZakirod <gabie1989@gmail.com>
2019-10-01 22:15:52 -0400

    signup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Signup from "./components/auth/Signup";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedInUser: null };
+  }
+
+  getTheUser = userObj => {
+    this.setState({
+      loggedInUser: userObj
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/signup"
+            render={() => <Signup getUser={this.getTheUser} />}
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
